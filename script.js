687111fd502d90c805776a63484f3eae58561582
Aurélien Gabert <gabertaurelien@gmail.com>
2021-04-08 15:54:21 +0200

    update

--- script.js
+++ script.js
@@ -1,25 +1,49 @@
 
 var button = document.getElementById('action');
-
-
+var list = document.getElementById("list");
 var input = document.getElementById("input");
-
-
-
-
+var item = document.getElementsByTagName("li") ;
 
 
 button.addEventListener("click", function() {
+    if (input.value !== ""){
+        var newList = document.createElement("li") ;
+        newList.appendChild(document.createTextNode(input.value)) ;
+        list.appendChild(newList) ;
 
-    var newList = document.createElement("li") ;
-    var list = document.getElementsByTagName("ul");
-    
-    newList.appendChild(document.createTextNode("testing")) ;
-
-    list.appendChild(newList)
-
-});
-
+        for (var i= 0; i<=item.length; i++){
+            item[i].addEventListener("click", function(){
+           console.log("fjrdslhbfrbjhf");
+                   })
+                   }
+        }
+    });
 
 
 
+input.addEventListener("keypress", function(event){
+    if (input.value !== "" && event.keyCode === 13){
+        var newList = document.createElement("li") ;
+        newList.appendChild(document.createTextNode(input.value)) ;
+        list.appendChild(newList) ;
+        }
+
+        for (var i= 0; i<=item.length; i++){
+             item[i].addEventListener("click", function(){
+            console.log("fjrdslhbfrbjhf");
+                    })
+                    }
+    });
+
+
+
+
+// for (var i= 0; i<=item.length; i++){
+//     item[i].addEventListener("click", function(){
+//         console.log("fjrdslhbfrbjhf");
+//     })
+
+//     for (var i= 0; i<=item.length; i++){
+//         item[i].addEventListener("click", function(){
+//             console.log("fjrdslhbfrbjhf");
+//         })
