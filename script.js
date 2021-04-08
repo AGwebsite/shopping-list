609c4cefef53e4926c7e54825777344c6e8b20fe
Aurélien Gabert <gabertaurelien@gmail.com>
2021-04-08 22:02:18 +0200

    check

--- script.js
+++ script.js
@@ -4,32 +4,30 @@ var list = document.getElementById("list");
 var input = document.getElementById("input");
 var item = document.getElementsByTagName("li") ;
 
+
 function addList(){
     if (input.value !== ""){
         addNewList() ;
-        checkList();
     }
 }
 
 function keyCode(event){
     if (input.value !== "" && event.keyCode === 13){
         addNewList();
-        checkList();
         }
 }
 
-function checkList (){
-    for (var i= 0; i<=item.length; i++){
-        item[i].addEventListener("click", function(){
-        console.log("test avant de mettre l'action cocher l'item");})
-                   }
-}
 
 function addNewList (){
     var newList = document.createElement("li") ;
+    newList.addEventListener("click", function(){
+        newList.classList.toggle('check')
+    })
     newList.appendChild(document.createTextNode(input.value)) ;
     list.appendChild(newList) ;
+    
 }
+
 
 
 button.addEventListener("click", addList);
